4c285dfb4d285d037cf7734b4e628e2b1aa13f6b
Amelin Vladislav <vamelin@ozon.ru>
2023-04-21 01:38:57 +0300

    resolve vpn

--- proxy/build.sh
+++ proxy/build.sh
@@ -1,1 +1,16 @@
-docker network create --subnet 172.20.0.0/24 wgnet_1
+# create network
+network_name='wgnet_1'
+
+if [[ $(docker network ls | grep $network_name) = "" ]]
+  then
+    echo 'creating network' $network_name '...'
+    docker network create --subnet 172.20.0.0/24 $network_name
+fi
+
+
+# ip route
+container_name='web'
+default_route_gateway='172.20.0.50'
+docker compose exec --privileged $container_name ip route del default
+echo 'setting up default route via'
+docker compose exec --privileged $container_name ip route add default via $default_route_gateway
